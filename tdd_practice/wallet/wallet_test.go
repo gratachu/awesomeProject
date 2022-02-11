54bd500157bf1c3c3144e380e3b2715255b3c951
gratachu <gratachu9@gmail.com>
2022-02-11 18:23:17 +0900

    feat: add withdraw insufficient pattern

--- tdd_practice/wallet/wallet_test.go
+++ tdd_practice/wallet/wallet_test.go
@@ -10,7 +10,17 @@ func TestWallet(t *testing.T) {
 		got := wallet.Balance()
 
 		if got != want {
-			t.Errorf("got %d want %d", got, want)
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 	t.Run("Deposit", func(t *testing.T) {
@@ -26,5 +36,13 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(20))
+		assertError(t, err, ErrInsufficientFunds)
 	})
 }
