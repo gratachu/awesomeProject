fab9b05d2c217fdd2113454c962130a3ac5e55d8
gratachu <gratachu9@gmail.com>
2022-03-12 20:44:53 +0900

    FEAT: add day17 second assignment

--- atcorder/day17/main.go
+++ atcorder/day17/main.go
@@ -7,9 +7,14 @@ import (
 
 func main() {
 	board := CreateBoard()
-	winner := JudgeVerticalLine(board)
+	ansL2R := CreateDiagonalLineLeft2Right(board)
+	ansR2L := CreateDiagonalLineRight2Left(board)
 
-	fmt.Println(winner)
+	if ansL2R == "O" || ansR2L == "X" {
+		fmt.Println(ansL2R)
+	} else {
+		fmt.Println(ansR2L)
+	}
 }
 
 func CreateBoard() []string {
@@ -23,19 +28,46 @@ func CreateBoard() []string {
 	return board
 }
 
-func JudgeVerticalLine(board []string) string {
-	var winner string
-	for i := 0; i < 5; i++ {
-		var line string
-		for _, s := range board {
-			ss := strings.Split(s, "")
-			line += ss[i]
-		}
+// func JudgeVerticalLine(board []string) string {
+// 	var winner string
+// 	for i := 0; i < 5; i++ {
+// 		var line string
+// 		for _, s := range board {
+// 			ss := strings.Split(s, "")
+// 			line += ss[i]
+// 		}
 
-		winner = CheckWinner(line)
+// 		winner = CheckWinner(line)
+// 	}
+
+// 	return winner
+// }
+
+// 左から右の斜めのラインを作る
+func CreateDiagonalLineLeft2Right(board []string) string {
+	var line string
+	for i := 0; i < 5; i++ {
+		str := board[i]
+		ss := strings.Split(str, "")
+		line += ss[i]
 	}
 
-	return winner
+	ans := CheckWinner(line)
+	return ans
+}
+
+// 右から左の斜めのラインを作る
+func CreateDiagonalLineRight2Left(board []string) string {
+	var line string
+
+	for i := 5; i > 0; i-- {
+		str := board[i-1]
+		ss := strings.Split(str, "")
+		line += ss[i-1]
+	}
+
+	ans := CheckWinner(line)
+	return ans
 }
 
 func CheckWinner(line string) string {
