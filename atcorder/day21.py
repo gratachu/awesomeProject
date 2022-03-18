d3f626d7d31cbde348f242516dac30de7ce78782
gratachu <gratachu9@gmail.com>
2022-03-18 10:19:08 +0900

    FEAT: add card game for two

---
+++ atcorder/day21.py
@@ -0,0 +1,16 @@
+## card game for two
+## https://atcoder.jp/contests/abs/tasks/abc088_b
+n = int(input())
+cards = list(map(int, input().split()))
+cards.sort(reverse=True)
+
+alice = 0
+bob = 0
+
+for i in range(len(cards)):
+    if i % 2 == 0:
+        alice += cards[i]
+    else:
+        bob += cards[i]
+
+print(alice - bob)
